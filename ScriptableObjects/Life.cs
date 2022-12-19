44738a4e6e626abbab04997f5074d4ef68809786
geoffery0 <justinscab@gmail.com>
2022-12-19 15:34:13 -0800

    finished commenting

--- ScriptableObjects/Life.cs
+++ ScriptableObjects/Life.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+// This is the base class for both Animal and Plant
     [CreateAssetMenu]
 public abstract class Life : ScriptableObject
 {
+    // These are the sprites
     public TileBase[] tile;
+    // This is how "old" it is
     public int age;
+    // This is how much energy it returns from being harvested
     public int energyReturn;
+    // Every life has an ID
     public int ID;
 
 
